68806b0e7709044d482bc09b45c0dbcb2209d650
2CPT1MP <2CPT1MP@gmail.com>
2019-10-09 15:44:18 +0300

    Established initial structed and started developing interative menu

---
+++ CP-Main/src/main.c
@@ -0,0 +1,11 @@
+#include "menu/Menu.h"
+
+
+int main()
+{
+	// Read from BINARY
+
+	ShowMenu();
+
+	// Save
+}
---
+++ CP-Main/src/menu/Menu.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define HEADER "========================================\n            FLIGHTS DATABASE\n========================================\n"
+
+const char* NOT_SELECTED[] = { "    Open text file", "    Display the sheet", "    Find a record", "    Save as text", "    Exit" };
+const char* SELECTED[] = { "=>  Open text file", "=>  Display the sheet", "=>  Find a record", "=>  Save as text", "=>  Exit" };
+
+void ShowMenu();
